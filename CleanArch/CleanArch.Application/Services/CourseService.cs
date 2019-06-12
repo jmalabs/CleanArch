788bc92039c0a3534bb1998460953b1aa812831a
jmalabs <johnrichard.malabanan.0916@gmail.com>
2019-06-12 16:34:57 +0800

    Implementation.

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository courseRepository;
+
+        public CourseService(ICourseRepository courseRepository)
+        {
+            this.courseRepository = courseRepository;
+        }
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = courseRepository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext context;
+
+        public CourseRepository(UniversityDBContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return context.Courses;
+        }
+    }
+}
